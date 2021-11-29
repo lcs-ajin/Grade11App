76cfbaf33e67b11bf744719d7e40ebcc2f54f9d4
Anthony Jin <ajin@lcs.0n.ca>
2021-11-29 14:25:56 -0500

    Initial Commit

---
+++ LcsLibrary/LcsLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LcsLibraryApp.swift
+//  LcsLibrary
+//
+//  Created by Anthony Jin on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct LcsLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
